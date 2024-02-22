145c9f5c219574f86529a86d1f0951e0a43195a9
Jason Lee <jlee220@gmail.com>
2024-02-21 20:31:10 -0600

    creating related entities and product entity

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,15 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity //using colon and file means we will deriving from this class.
+                                      //now it's grabbing the Id from that file and removing it from here
     {
-        public int Id { get; set; } //this will become the primary key. must be Id. not TheId or some other name
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public ProductType ProductType { get; set; }      
+        public int ProductTypeId { get; set; }  
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
 }

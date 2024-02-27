0f33b6466bb1bddf74affc94d5269fddcbfe78a8
Jason Lee <jlee220@gmail.com>
2024-02-26 22:46:06 -0600

    add evaluator. wtf is going on

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -2,7 +2,7 @@ using System.Linq.Expressions;
 
 namespace Core.Specifications
 {
-    public class ISpecification<T>
+    public interface ISpecification<T>
     {
         Expression<Func<T, bool>> Criteria {get; }
         List<Expression<Func<T, object>>> Includes {get; }

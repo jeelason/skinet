d5bf37c501aed22e159eb440849541f7a0938175
Jason Lee <jlee220@gmail.com>
2024-02-26 23:14:26 -0600

    using the specification methods in the controller. absolutely miffed.

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -11,7 +11,7 @@ namespace Infrastructure.Data
         {
             var query = inputQuery;
             if (spec.Criteria != null) {
-                query = query.Where(spec.Criteria);
+                query = query.Where(spec.Criteria); // ex. where p => p.ProductTypeId == id
             }
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
             return query;

02fc59f461adf2e72f00428b755e276238296550
Jason Lee <jlee220@gmail.com>
2024-03-06 21:33:41 -0600

    subscribe to product data

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +7,10 @@ import { Pagination } from './shared/models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
   
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({  //using this syntax alot. we must subscribe when using an observable. needs something to listen to it
-      next: response => this.products = response.data, //what to do next
-      error: error => console.log(error), //what to do if there is an error
-      complete: () => {
-        console.log('request completed');
-        console.log('extra statement');
-      }
-    })
+
   }
 }

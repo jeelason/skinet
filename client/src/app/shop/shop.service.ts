ae332cffd4803bd3b0e0a0acf8910e447f638313
Jason Lee <jlee220@gmail.com>
2024-03-06 22:19:28 -0600

    product filters added, need functionality

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
+import { Type } from '../shared/models/type';
+import { Brand } from '../shared/models/brand';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,13 @@ export class ShopService {
   constructor(private http: HttpClient) { }
   getProducts() {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands() {
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<Type[]>(this.baseUrl + 'products/types');
   }
 }

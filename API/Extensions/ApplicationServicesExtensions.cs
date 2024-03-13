ded70b6c63384154399e52fe791832e7f9171527
Jason Lee <jlee220@gmail.com>
2024-03-12 19:58:38 -0500

    create basket repository interface

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -24,6 +24,7 @@ namespace API.Extensions
                 var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
                 return ConnectionMultiplexer.Connect(options);
             });
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericReopository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());

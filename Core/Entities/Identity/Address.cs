0cedb91e5c845a1a35ae2039fccd1452243e4bf1
Jason Lee <jlee220@gmail.com>
2024-03-21 22:21:42 -0500

    adding new migration

--- Core/Entities/Identity/Address.cs
+++ Core/Entities/Identity/Address.cs
@@ -13,7 +13,7 @@ namespace Core.Entities.Identity
         public string ZipCode { get; set; }
 
         [Required]
-        public string AppUsedId { get; set; }
+        public string AppUserId { get; set; }
         public AppUser AppUser { get; set; }
     }
 }

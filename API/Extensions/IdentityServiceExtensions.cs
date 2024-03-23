c3ef648a80f902a7d80c32346487862f8a22b2f8
Jason Lee <jlee220@gmail.com>
2024-03-22 20:07:21 -0500

    add startup services for identity

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,6 @@
+using Core.Entities.Identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -11,6 +13,17 @@ namespace API.Extensions
             services.AddDbContext<AppIdentityDbContext>(opt => {
                 opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
             });
+
+            services.AddIdentityCore<AppUser>(opt => 
+            {
+                //add identity options here
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+            services.AddAuthorization();
+
             return services;
         }
     }

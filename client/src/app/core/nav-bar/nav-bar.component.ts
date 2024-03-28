1998b3a06bb96b06522074f3f3e2665e9f99ca99
Jason Lee <jlee220@gmail.com>
2024-03-27 19:10:47 -0500

    update nav bar for logged in user

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { BasketItem } from 'src/app/shared/models/baskets';
 
@@ -9,7 +10,7 @@ import { BasketItem } from 'src/app/shared/models/baskets';
 })
 export class NavBarComponent {
 
-  constructor(public basketService: BasketService) {}
+  constructor(public basketService: BasketService, public accountService: AccountService) {}
 
   getCount(items: BasketItem[]) {
     return items.reduce((sum, item) => sum + item.quantity, 0)

ede8236f59e3c47ab48018aaf62eca300f0de70e
Jason Lee <jlee220@gmail.com>
2024-04-07 20:34:16 -0500

    shipping price finally persisting

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -15,8 +15,6 @@ export class BasketService {
   basketSource$ = this.basketSource.asObservable();
   private basketTotalSource = new BehaviorSubject<BasketTotals | null>(null);
   basketTotalSource$ = this.basketTotalSource.asObservable();
-  shipping = 0;
-
 
   constructor(private http: HttpClient) { }
 
@@ -25,15 +23,14 @@ export class BasketService {
       .pipe(
         map(basket => {
           this.basketSource.next(basket);
-          console.log(basket);
         })
       )
   }
 
   setShippingPrice(deliveryMethod: DeliveryMethod) {
     const basket = this.getCurrentBasketValue();
-    this.shipping = deliveryMethod.price;
     if (basket) {
+      basket.shippingPrice = deliveryMethod.price;
       basket.deliveryMethodId = deliveryMethod.id;
       this.setBasket(basket);
     }
@@ -127,8 +124,8 @@ export class BasketService {
     const basket = this.getCurrentBasketValue();
     if (!basket) return;
     const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
-    const total = subtotal + this.shipping;
-    this.basketTotalSource.next({shipping: this.shipping, total, subtotal});
+    const total = subtotal + basket.shippingPrice;
+    this.basketTotalSource.next({shipping: basket.shippingPrice, total, subtotal});
   }
 
   private isProduct(item: Product | BasketItem) : item is Product {

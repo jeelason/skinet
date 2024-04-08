8419cf02730b3be30da58c580395e6d059151d4f
Jason Lee <jlee220@gmail.com>
2024-04-07 21:13:43 -0500

    display card validation errors

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -22,6 +22,7 @@ export class CheckoutPaymentComponent implements OnInit {
   cardNumber?: StripeCardNumberElement;
   cardExpiry?: StripeCardExpiryElement;
   cardCvc?: StripeCardCvcElement;
+  cardErrors: any;
   stripeTestKey: string = 'pk_test_51P20omK2vuGvZ84FKVitm74UmND871SDmuNc5YxaHI6rezLfcGtCnOcnKeIGZuLNVE4OTNEEMbPdbpiHuTwQrVug00V53UGVz4';
 
   constructor(private basketService: BasketService, private checkoutService: CheckoutService,
@@ -34,6 +35,24 @@ export class CheckoutPaymentComponent implements OnInit {
       if (elements) {
         this.cardNumber = elements.create('cardNumber');
         this.cardNumber.mount(this.cardNumberElement?.nativeElement);
+        this.cardNumber.on('change', event => {
+          if (event.error) this.cardErrors = event.error.message;
+          else this.cardErrors = null;
+        })
+
+        this.cardExpiry = elements.create('cardExpiry');
+        this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);
+        this.cardExpiry.on('change', event => {
+          if (event.error) this.cardErrors = event.error.message;
+          else this.cardErrors = null;
+        })
+
+        this.cardCvc = elements.create('cardCvc');
+        this.cardCvc.mount(this.cardCvcElement?.nativeElement);
+        this.cardCvc.on('change', event => {
+          if (event.error) this.cardErrors = event.error.message;
+          else this.cardErrors = null;
+        })
 
         this.cardExpiry = elements.create('cardExpiry');
         this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);

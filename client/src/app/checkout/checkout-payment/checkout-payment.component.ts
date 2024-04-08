09d3b7cce44baf83edb73cdaecccebf3f00c8e17
Jason Lee <jlee220@gmail.com>
2024-04-07 21:35:01 -0500

    test card failures with stripe dummy cards

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -84,6 +84,8 @@ export class CheckoutPaymentComponent implements OnInit {
             this.basketService.deleteLocalBasket();        
             const navigationExtras: NavigationExtras = {state: order};
             this.router.navigate(['checkout/success'], navigationExtras);
+          } else {
+            this.toastr.error(result.error.message);
           }
         })        
       }
